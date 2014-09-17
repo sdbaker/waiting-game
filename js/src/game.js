023ef50cb1619a2a7bd0a1ded9ac9553243330bc
jbakerdev <jbakerdev@gmail.com>
2014-09-16 20:06:44 -0600

    Stuff

---
+++ js/src/game.js
@@ -0,0 +1,12 @@
+var waitingGameApp = {
+    preload: function(){
+
+    },
+    //Should be called load but w/e
+    create: function(){
+
+    },
+    update: function(){
+
+    }
+}
--- js/src/main.js
+++ js/src/main.js
@@ -1,1 +1,3 @@
-alert('yourFace');
+var gameInstance = new Phaser.Game(640, 480, Phaser.AUTO, 'appRoot');
+gameInstance.state.add('main', waitingGameApp);
+gameInstance.state.start('main');
